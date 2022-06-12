44bcda88c192db4f7e8946e936aad82b44619e3f
Markmutisya <mark.mwangi@dtudent.moringaschool.com>
2022-06-12 10:32:54 +0300

    Find

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(arrayTesting){
+    let winning =(arrayTesting) => arrayTesting.result === 'W';
+    let Kushinda = arrayTesting.find(winning)
+    if(Kushinda)
+    {
+        return Kushinda.year
+    }
+    else {
+        return undefined;
+    }
+}
